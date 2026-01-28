219761bf0fe0e12e58c2c6ab28d1c4510ea724d9
João Lucena <iaolucena@gmail.com>
2026-01-27 21:27:21 -0300

    Initial Commit

---
+++ TRIA MED/TRIA_MEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRIA_MEDApp.swift
+//  TRIA MED
+//
+//  Created by Joao felipe Lucena on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TRIA_MEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TRIA MEDTests/TRIA_MEDTests.swift
@@ -0,0 +1,17 @@
+//
+//  TRIA_MEDTests.swift
+//  TRIA MEDTests
+//
+//  Created by Joao felipe Lucena on 27/01/26.
+//
+
+import Testing
+@testable import TRIA_MED
+
+struct TRIA_MEDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
